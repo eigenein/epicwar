3fbefc9f39e47b82b4c4566a3473c8953772e899
Pavel Perestoronin <pavel.perestoronin@booking.com>
2016-07-28 22:30:37 +0200

    Initial request making code (with invalid signature)

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import contextlib
+import hashlib
+import json
 import logging
+import random
+import string
+import time
+import typing
 
 import click
 import requests
@@ -11,10 +18,62 @@ class EpicWar:
     """
     Epic War API.
     """
-    BASE_URL = "https://epicwar-vkontakte.progrestar.net/api"
-
-    def __init__(self):
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+        self.auth_token = "TODO"
         self.session = requests.Session()
+        self.session_id = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(14))
+        self.request_id = 0
+
+    def post(self, name: str, **args) -> dict:
+        """
+        Makes request to the game API.
+        """
+        logging.debug("%s(%s)", name, args)
+        self.request_id += 1
+        data = json.dumps({"session": None, "calls": [{"ident": "group_0_body", "name": name, "args": args}]})
+        headers = {
+            "Referer": "https://epicwar.cdnvideo.ru/vk/v0290/assets/EpicGame.swf",
+            "Content-type": "application/json; charset=UTF-8",
+            "X-Auth-Token": self.auth_token,
+            "X-Auth-Network-Ident": "vkontakte",
+            "X-Auth-Session-Id": self.session_id,
+            "X-Requested-With": "XMLHttpRequest",
+            "X-Request-Id": str(self.request_id),
+            "X-Auth-User-Id": self.user_id,
+            "X-Env-Library-Version": "0",
+            "X-Server-Time": int(time.time()),
+            "X-Auth-Application-Id": "3644106",
+            "Content-length": len(data),
+        }
+        if self.request_id == 1:
+            headers["X-Auth-Session-Init"] = "1"
+        headers["X-Auth-Signature"] = self.get_signature(data, headers)
+        response = self.session.post("https://epicwar-vkontakte.progrestar.net/api/", data=data, headers=headers)
+        logging.debug("%s", response.text)
+        return response.json()
+
+    @staticmethod
+    def get_signature(data: str, headers: typing.Dict[str, typing.Any]):
+        fingerprint = "".join(
+            "{}={}".format(*pair)
+            for pair in sorted(
+                (key[6:].upper(), value)
+                for key, value in headers.items()
+                if key.startswith("X-Env")
+            )
+        )
+        data = ":".join((
+            headers["X-Request-Id"],
+            headers["X-Auth-Token"],
+            headers["X-Auth-Session-Id"],
+            data,
+            fingerprint,
+        )).encode("utf-8")
+        return hashlib.md5(data).hexdigest()
+
+    def close(self):
+        self.session.close()
 
 
 class ColorStreamHandler(logging.StreamHandler):
@@ -36,12 +95,19 @@ class ColorStreamHandler(logging.StreamHandler):
         return click.style(super().format(record), fg=self.COLORS[record.levelno])
 
 
+class ContextObject:
+    user_id = None  # type: str
+
+
 @click.group()
 @click.option("-v", "--verbose", help="Log debug info.", is_flag=True)
-def main(verbose: True):
+@click.option("-u", "--user-id", help="VKontakte user ID.", required=True)
+@click.pass_obj
+def main(obj: ContextObject, verbose: True, user_id: str):
     """
     Epic War bot.
     """
+    obj.user_id = user_id
     handler = ColorStreamHandler(click.get_text_stream("stderr"))
     handler.setFormatter(logging.Formatter(
         fmt="%(asctime)s [%(levelname).1s] %(message)s",
@@ -53,12 +119,14 @@ def main(verbose: True):
 
 
 @main.command()
-def run():
+@click.pass_obj
+def run(obj: ContextObject):
     """
     Run the bot.
     """
-    pass
+    with contextlib.closing(EpicWar(obj.user_id)) as epic_war:
+        epic_war.post("getUsersInfo", users=["325351204","280409989"])
 
 
 if __name__ == "__main__":
-    main()
+    main(obj=ContextObject())
