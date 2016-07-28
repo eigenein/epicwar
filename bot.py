5e03a136205578cd7fb9242c87fbca5ab85a9b57
Pavel Perestoronin <pavel.perestoronin@booking.com>
2016-07-28 23:34:17 +0200

    Pass auth token via command line parameter

--- bot.py
+++ bot.py
@@ -18,9 +18,9 @@ class EpicWar:
     """
     Epic War API.
     """
-    def __init__(self, user_id: str):
+    def __init__(self, user_id: str, auth_token: str):
         self.user_id = user_id
-        self.auth_token = "TODO"
+        self.auth_token = auth_token
         self.session = requests.Session()
         self.session_id = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(14))
         self.request_id = 0
@@ -29,8 +29,8 @@ class EpicWar:
         """
         Makes request to the game API.
         """
-        logging.debug("%s(%s)", name, args)
         self.request_id += 1
+        logging.debug("#%s %s(%s)", self.request_id, name, args)
         data = json.dumps({"session": None, "calls": [{"ident": "group_0_body", "name": name, "args": args}]})
         headers = {
             "Referer": "https://epicwar.cdnvideo.ru/vk/v0290/assets/EpicGame.swf",
@@ -48,13 +48,17 @@ class EpicWar:
         }
         if self.request_id == 1:
             headers["X-Auth-Session-Init"] = "1"
-        headers["X-Auth-Signature"] = self.get_signature(data, headers)
-        response = self.session.post("https://epicwar-vkontakte.progrestar.net/api/", data=data, headers=headers)
+        headers["X-Auth-Signature"] = self.sign_request(data, headers)
+        response = self.session.post(
+            "https://epicwar-vkontakte.progrestar.net/api/", data=data, headers=headers)
         logging.debug("%s", response.text)
-        return response.json()
+        return response.json()["results"][0]
 
     @staticmethod
-    def get_signature(data: str, headers: typing.Dict[str, typing.Any]):
+    def sign_request(data: str, headers: typing.Dict[str, typing.Any]):
+        """
+        Generates X-Auth-Signature header value.
+        """
         fingerprint = "".join(
             "{}={}".format(*pair)
             for pair in sorted(
@@ -97,22 +101,27 @@ class ColorStreamHandler(logging.StreamHandler):
 
 class ContextObject:
     user_id = None  # type: str
+    auth_token = None  # type: str
 
 
 @click.group()
 @click.option("-v", "--verbose", help="Log debug info.", is_flag=True)
 @click.option("-u", "--user-id", help="VKontakte user ID.", required=True)
+@click.option("-t", "--token", help="Epic War authentication token.", required=True)
 @click.pass_obj
-def main(obj: ContextObject, verbose: True, user_id: str):
+def main(obj: ContextObject, verbose: True, user_id: str, token: str):
     """
     Epic War bot.
     """
     obj.user_id = user_id
+    obj.auth_token = token
+
     handler = ColorStreamHandler(click.get_text_stream("stderr"))
     handler.setFormatter(logging.Formatter(
         fmt="%(asctime)s [%(levelname).1s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     ))
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO if not verbose else logging.DEBUG)
     logger.addHandler(handler)
@@ -124,8 +133,8 @@ def run(obj: ContextObject):
     """
     Run the bot.
     """
-    with contextlib.closing(EpicWar(obj.user_id)) as epic_war:
-        epic_war.post("getUsersInfo", users=["325351204","280409989"])
+    with contextlib.closing(EpicWar(obj.user_id, obj.auth_token)) as epic_war:
+        epic_war.post("collectResource", buildingId=4)
 
 
 if __name__ == "__main__":
