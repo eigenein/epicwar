beefda31b6e77b9e34f29e9209b3f67197d05767
Pavel Perestoronin <eigenein@gmail.com>
2016-08-11 18:06:17 +0200

    Improve notification text

--- bot.py
+++ bot.py
@@ -889,7 +889,7 @@ class Bot:
         logging.info("Sending Telegram notification…")
         if self.incomplete_buildings:
             construction = ", ".join(
-                "*{}* by *{}*".format(building.type.name, datetime.datetime.fromtimestamp(building.complete_time))
+                "*{}* by *{:%b %d %-H:%M}*".format(building.type.name, datetime.datetime.fromtimestamp(building.complete_time))
                 for building in self.incomplete_buildings
             )
         else:
