6af0b83b273bd96bd68120951d260421ea002af3
Pavel Perestoronin <eigenein@gmail.com>
2016-10-10 01:36:25 +0200

    Fix docstring

--- epicbot/enums.py
+++ epicbot/enums.py
@@ -3,10 +3,6 @@
 
 """
 Game enumerations.
-
-Epic War has a lot of unit, building and resource types. Some of them are temporary and only
-use during some game events. Thus, some values are added explicitly to be able to use them and
-the others are generated to cover the range of IDs.
 """
 
 import enum
